1b7d9b73fccf62a58033cb10f17f1b713e09168f
José Jhovanny Téllez Martínez <jose.tellezm@te.gob.mx>
2022-10-13 12:23:38 -0500

    Change Styles

--- invitacion.js
+++ invitacion.js
@@ -88,7 +88,7 @@ function initMap() {
 }, 1000);
 
 
-let slideIndex = 1;
+// let slideIndex = 1;
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -114,8 +114,27 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+// let slideIndex = 0;
+// showSlides();
 
-//showSlides(slideIndex);
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 4000); // Change image every 2 seconds
+// }
+
+showSlides(slideIndex);
 
 
 
